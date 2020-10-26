51017a984404038230882b1bf0aec24fa8e884f5
subbus <subbareddyg83@gmail.com>
2020-10-26 09:36:07 +0530

    added validation of  first name

--- user-registration.sh
+++ user-registration.sh
@@ -1,2 +1,33 @@
 #!/bin/bash
-echo "Welcome to user registration problem!"
+while [[ True ]]
+do
+    echo "--------------------------------------"
+    echo "1 for entering first name "
+    echo "5 for exit"
+    read -p "enter your option:" option
+    
+    case $option in 
+        1)
+            pattern="^[A-Z][a-zA-Z]{2,}$"
+            ;;
+        5)
+            echo "exiting......"
+            exit
+            ;;
+        *)
+            echo 'enter valid option'
+            ;;
+    esac
+
+    if [[ $option =~ ^[1]$ ]]
+    then
+        read -p "enter your input:" input
+        if [[ $input =~ $pattern ]]
+        then
+            echo "$input is valid"
+        else
+            echo "$input is not valid"
+        fi
+    fi
+    
+done 
